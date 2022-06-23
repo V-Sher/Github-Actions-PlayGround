779e462e2eaf056230fa9bd1f573e0789875c1b1
V-Sher <varshitasher@gmail.com>
2022-06-23 16:01:21 +0100

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,5 +3,5 @@ from utils import create_array
 
 def test_equal_arrays():
   arr1 = create_array()
-  arr2 = create_array()
+  arr2 = create_array()*2
   assert (np.array_equal(arr1, arr2)), "Arrays are not equal"
