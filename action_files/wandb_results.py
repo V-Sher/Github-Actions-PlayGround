82da32790a74eb119ee45c8cd0b8374069a716f6
V-Sher <varshitasher@gmail.com>
2022-06-30 12:10:52 +0100

    reformat csv

--- action_files/wandb_results.py
+++ action_files/wandb_results.py
@@ -20,15 +20,15 @@ def reformat_df(df):
     # add links to run id
     df['Run ID'] = df.apply(lambda x: "["+x['run.id']+"]("+x['run.url'] +")", axis=1)
     # select columns
-    clean_df = df[['__eval.category', 'Run ID', 'github_sha', 'loss', 'best_val_loss', 'accuracy', 'best_val_acc', '_runtime']].round(3)
+    clean_df = df[['__eval.category', 'Run ID', 'eval/loss', 'eval/accuracy', '_num_train_epochs', '_num_hidden_layers'].round(3)
     # rename columns
-    clean_df.columns=['Category', 'Run ID', 'SHA', 'Train Loss', 'Val Loss', 'Acc', 'Val Acc', 'Runtime']
+    clean_df.columns=['Category', 'Run ID', 'Val Loss', 'Val Acc', 'Epochs', 'Hidden Layers']
     # emphasize candidate runs
     clean_df.loc[clean_df.Category == 'candidate'] = clean_df[clean_df.Category == 'candidate'].apply(lambda x: ['**'+str(x)+'**' for x in x.values])
     return clean_df
 
-# clean_df = reformat_df(df)
-clean_df = df
+clean_df = reformat_df(df)
+# clean_df = df
 table = tabulate(clean_df, tablefmt="github", headers="keys", showindex=False)
 message = f"### Model Evaluation Results\n\n{table}\n\nResults queried from W&B project [{entity_name}/{proj_name}]({proj_url})"
 
@@ -41,15 +41,3 @@ data = {'body': f"{message}"}
 result = requests.post(url=url, headers=headers, json=data)
 
 assert result.status_code == 201, f'Data summary did not post to PR successfully, received error code: {result.status_code}'
-
-# # Remove Label
-# headers = {'Accept': 'application/vnd.github.symmetra-preview+json',
-#            'Authorization': f'token {token}'}
-# url = f"https://api.github.com/repos/{nwo}/issues/{pr_num}/labels/Full%20Test%20Pending"
-
-# result = requests.delete(url=url, headers=headers)
-
-# # Add Label
-# url = f"https://api.github.com/repos/{nwo}/issues/{pr_num}/labels"
-# data = {"labels": ["Full Test Complete"]}
-# result = requests.post(url=url, headers=headers, json=data)
